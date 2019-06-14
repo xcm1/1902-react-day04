0d6674e63af17bc0fb0e970d47cf44fa61e4249b
xcm1 <121969209@qq.com>
2019-06-14 16:58:22 +0800

    父子组件传值

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Com extends Component {
+  render() {
+    return (
+      <div className="box">
+        <header className="header">首页头部</header>
+        <div className="content">首页内容</div>
+      </div>
+    )
+  }
+}
+export default Com;
