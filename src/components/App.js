87aff5377b801cbb1bca19f73426ecbce8a9bd64
xcm1 <121969209@qq.com>
2019-06-14 20:10:26 +0800

    函数式

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
+import Home from '@/components/Home';
+import '@/main';
 
 function fn (user) {
-  return user.firstname + ' ' + user.lastname;
+  return user.firstname + " " + user.lastname;
 }
-
 const user = {
-  firstname: "wu",
+  firstname: 'wu',
   lastname: 'daxun'
 }
 
 const Com = <div>hello, { fn(user) }</div>
-
 export default Com;
