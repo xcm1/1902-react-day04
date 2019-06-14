983f09a12eda632c23e0a819b673caaab843ce48
xcm1 <121969209@qq.com>
2019-06-14 17:16:58 +0800

    父给子传值

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import ProList from './ProList'
 class Com extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      prolist: []
+    }
+  }
+
+  componentDidMount () {
+    fetch('http://www.daxunxun.com/douban').then(res => res.json().then(data => {
+      this.setState({
+        prolist: data
+      })
+    }))
+  }
+
   render() {
     return (
       <div className="box">
         <header className="header">首页头部</header>
         <div className="content">
-          <ProList />
-          首页内容</div>
+          首页内容
+          <ProList prolist = { this.state.prolist }/>
+        </div>
       </div>
     )
   }
