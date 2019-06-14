783152cd955433d5834b62fbf83e408756725738
xcm1 <121969209@qq.com>
2019-06-14 17:05:35 +0800

    prolist组件

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-
+import ProList from './ProList'
 class Com extends Component {
   render() {
     return (
       <div className="box">
         <header className="header">首页头部</header>
-        <div className="content">首页内容</div>
+        <div className="content">
+          <ProList />
+          首页内容</div>
       </div>
     )
   }
---
+++ src/components/ProList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Com extends Component {
+  render () {
+    return (
+      <ul>
+        <li>
+          肖申克的救赎
+        </li>
+      </ul>
+    )
+  }
+}
+
+export default Com;
